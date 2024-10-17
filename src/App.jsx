42b907a8f6ec4d0df4db9a88e08793332871af7d
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-10-18 00:49:42 +0530

    Fixed auth issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css'
 import { Auth } from './pages/Auth'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Chat from './pages/Chat'
+import { auth } from '../config/firebase-config';
+
+function PrivateRoute({ children }) {
+  const currentUser = auth.currentUser;
+  
+  return currentUser ? children : <Navigate to="/" />;
+}
+
 function App() {
 
   return (
@@ -9,7 +17,9 @@ function App() {
         <div className='app'>
           <Routes>
             <Route path="/" element={<Auth/ >} />
-            <Route path="/chat" element={<Chat/ >} />
+            <Route path="/chat" element={<PrivateRoute>
+                <Chat />
+              </PrivateRoute>} />
           </Routes>
         </div>
     </Router>
